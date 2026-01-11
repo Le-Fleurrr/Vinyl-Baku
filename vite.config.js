8e6a71b2e13657d71d6ede6627c8d9c90eaddd2a
McKnight <babaevtamerlan3@gmail.com>
2026-01-11 15:55:55 +0400

    Alpha Build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Vinyl-Baku/',
+  base: '/Backrooms/',
   plugins: [react()],
 });
